fc4d608b53393715aff1c15cb449a36f61cf8647
oscarnovillo <oscar.novillo@gmail.com>
2024-02-10 13:00:19 +0100

    demokotlin

---
+++ demoKotlin/demoKotlin/src/main/kotlin/org/example/demokotlin/DemoKotlinApplication.kt
@@ -0,0 +1,11 @@
+package org.example.demokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoKotlinApplication>(*args)
+}
